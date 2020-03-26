60e283342791db4ebf7a0e54f89df26b4d18e3d8
Pragna97 <62683965+Pragna97@users.noreply.github.com>
2020-03-26 16:21:15 +0530

    Update DEMO1.java

--- GitExample1/src/com/ntt/demo/DEMO1.java
+++ GitExample1/src/com/ntt/demo/DEMO1.java
@@ -1,11 +1,14 @@
 package com.ntt.demo;
-
+import java.util.Scanner;
 public class DEMO1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Wellcome");
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name=sc.next();
+		
+		System.out.println("WELCOME "+name.toUpperCase()+" to GIT TUTORRIALS!!");
 	}
 
 }
